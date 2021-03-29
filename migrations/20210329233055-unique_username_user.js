b210886c35f8f1ea7037984be7311e4266c7e96a
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-29 17:43:23 -0600

    Generated migration to change username for user to unique.

---
+++ migrations/20210329233055-unique_username_user.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.changeColumn('users', 'username', {
+      type: Sequelize.STRING,
+      unique: true
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.changeColumn('users', 'username')
+  }
+};
