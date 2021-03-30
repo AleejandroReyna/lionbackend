966512bdfe293c6c90af912bf03fe1976acd7e99
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-30 10:29:53 -0600

    Changed response type for data service

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -5,8 +5,18 @@ exports.getData = async (req, res) => {
   try {
     let request = await fetch("https://dog.ceo/api/breeds/list/all", {method: 'get'})
     if (request.status === 200) {
+      let _response = []
       let response = await request.json()
-      return res.json(response)
+      let _data = {...response.message}
+      for (const p in _data) {
+        let newBreed = {name: p}
+        if(_data[p].length > 0) {
+          newBreed.subBreeds = []
+          _data[p].map(sub => newBreed.subBreeds.push({name: sub}))
+        }
+        _response.push(newBreed)
+      }
+      return res.json(_response)
     }
     return res.status(request.status).send({ errors })
   } catch(e) {
