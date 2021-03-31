4a6b5d1a47dbbebc9ad583deb9e821d00554b699
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-03-30 23:48:11 -0600

    Added view for create Favorite Breed to user.

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -1,4 +1,6 @@
+const db = require("../models")
 const fetch = require('node-fetch')
+const FavoriteBreed = db.FavoriteBreed
 
 exports.getData = async (req, res) => {
   let errors = []
@@ -27,5 +29,33 @@ exports.getData = async (req, res) => {
 
 exports.setFavorite = async (req, res) => {
   let errors = []
-  res.json({h: "w"})
+  !req.body.breed ? errors.push({field: 'breed', error: 'The field is required!'}) : null
+  if (errors.length > 0) {
+    return res.status(400).send({
+      errors
+    })
+  }
+  let params = {breed: req.body.breed, userId: req.user.data.id, parent: null}
+  if(req.body.parent) {
+    params['parent'] = req.body.parent
+  }
+  //upsert doesn't work for me
+  try {
+    let request = await FavoriteBreed.findOne({userId: params.userId})
+    if (request) {
+      console.log(request.id)
+      let updateRequest = await FavoriteBreed.update(params, {
+        where: {
+          id: request.id
+        }
+      })
+      return res.json({id: request.id, status: "changed"})
+    } else {
+      let createRequest = await FavoriteBreed.create(params)
+      return res.json({id: createRequest.id, status: "created"})
+    }
+  }catch(e) {
+    errors.push({field: "", error: `${e}`})
+    return res.status(500).send({ errors })
+  }
 }
